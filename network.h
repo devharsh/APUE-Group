b91c07775f88d64298d45c64e7d133088e202abe
athiban p <athibanparthiban@gmail.com>
2019-11-22 13:48:34 -0500

    Included prototypes for directory indexing

--- network.h
+++ network.h
@@ -15,12 +15,8 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-<<<<<<< HEAD
 #include <dirent.h>
 #include <fts.h>
-=======
-#include <limits.h>
->>>>>>> f04e1aa48489313cb67f4d2bae176bc90828899a
 
 #define TRUE 1
 #define BUFFERSIZE 16384
@@ -54,13 +50,14 @@ struct request {
                       | "503"   ; Service Unavailable
                       | extension-code
 */
+
 struct response {
-    char *date;
-    char *server;
-    char *last_modified;
-    char *content_type;
-    int  content_length;
-    char *data;
+    char *date; // server date
+    char *server; // sws 
+    char *last_modified; // directory last modified
+    char *content_type; // depends
+    int  content_length; // calculate
+    char *data; // depends
 };
 
 struct server_information {
@@ -73,12 +70,8 @@ struct server_information {
 FILE* fp;
 
 int msgsock;
-<<<<<<< HEAD
-int open_connection(struct sockaddr *server, int protocol);
-int (*compar) (const FTSENT **, const FTSENT **);
-=======
 int open_connection(struct sockaddr *server, struct server_information);
->>>>>>> f04e1aa48489313cb67f4d2bae176bc90828899a
+int (*compar) (const FTSENT **, const FTSENT **);
 int handle_child_request();
 void handle_child_process(__attribute__((unused)) int signal);
 int add_line_to_request(char *request, char *line, unsigned int buffersize);
@@ -88,11 +81,6 @@ bool parse_first_line(char *line, struct request *req);
 bool validate_additional_information(char *line, struct request *req);
 bool validate_date(char* date_str, struct request *req);
 bool validate_tm(struct tm *time_ptr);
-<<<<<<< HEAD
 int traverse_files(struct request *req);
 int sortLexographical(const FTSENT **fileEntryPointer, const FTSENT **fileEntryPointerTwo);
 void generate_html(char* data);
-=======
-void set_env(char *key, char *value);
-char* get_hostname();
->>>>>>> f04e1aa48489313cb67f4d2bae176bc90828899a
