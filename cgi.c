a26f91b2fb07e77c92952a08d64d23e0cebcc7c0
Devharsh Trivedi <devharsh@live.in>
2019-12-01 20:54:15 -0500

    free hostname and port buffers in set_environment()

--- cgi.c
+++ cgi.c
@@ -159,8 +159,8 @@ set_environment(struct request *req, struct response *res, struct server_informa
     int env_index = 0;
     
     /*
-    * PATH_TRANSLATED - request
-    */
+     * PATH_TRANSLATED - request
+     */
    if ((hostname = malloc(_POSIX_HOST_NAME_MAX)) == NULL) {
         fprintf(stderr, "Could not allocate memory 7: %s\n", strerror(errno));
         return NULL;
@@ -236,6 +236,9 @@ set_environment(struct request *req, struct response *res, struct server_informa
 
     environment[env_index++] = '\0';
     
+    (void) free(hostname);
+    (void) free(port);
+    
     return environment;
 }
 
@@ -261,7 +264,7 @@ get_env_string(char *key, char *value) {
         return NULL;
     }
 
-    free(buffer);
+    (void) free(buffer);
     return result;
 }
 
